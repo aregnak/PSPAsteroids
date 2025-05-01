98ccb7be026927983d39e1a84d85d37a9fdbd6f7
areg <areg.nakashian@gmail.com>
2025-05-01 01:12:11 -0400

    Updated upload.sh

--- upload.sh
+++ upload.sh
@@ -2,16 +2,19 @@
 
 set -e
 
+# Replace with your PSP mount path
+PSP_PATH="/run/media/areg/disk/"
+
 cd ./build
 
 make
 
 echo "Removing old EBOOT.PBP"
-if (rm /run/media/areg/disk/PSP/GAME/Asteroids/EBOOT.PBP) then
+if (rm $PSP_PATH/PSP/GAME/Asteroids/EBOOT.PBP) then
     echo "Successfully Removed old EBOOT.PBP file"
 
     echo "Uploading new EBOOT.PBP file"
-    if (cp ./EBOOT.PBP /run/media/areg/disk/PSP/GAME/Asteroids/EBOOT.PBP) then
+    if (cp ./EBOOT.PBP $PSP_PATH/PSP/GAME/Asteroids/EBOOT.PBP) then
         echo "Successfully uploaded, enjoy the game!"
     fi
 fi
